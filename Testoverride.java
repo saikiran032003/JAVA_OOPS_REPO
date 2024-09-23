278da0c18fc5799dbf702fc8bc7bfadeeae91652
saikiran032003 <165379895+saikiran032003@users.noreply.github.com>
2024-09-24 00:59:30 +0530

    Add files via upload

---
+++ Testoverride.java
@@ -0,0 +1,20 @@
+class Override{
+    void method(){
+        System.out.println("chanting lord rama is very powerful");
+    }
+}
+class Override1 extends Override{
+    void method(){
+        System.out.println("chanting lord krishna can solve our problems");
+    }
+}
+class Testoveride{
+    public static void main(String args[]){
+        Override m1=new Override();
+        m1.method();
+        Override1 m2=new Override1();
+        m2.method();
+        Override m3=new Override1();
+        m3.method();
+    }
+}
