caaedf8c5368b33bfa48e3decf4c781318e4277e
saikiran032003 <165379895+saikiran032003@users.noreply.github.com>
2024-09-24 00:50:03 +0530

    Add files via upload

---
+++ Testoverloading.java
@@ -0,0 +1,18 @@
+class overloading{
+    public void add(int a,int b){
+        System.out.println(a+b);}
+        public void add(int a,int b,int c){
+            System.out.println(a+b+c);
+        }
+        public void add(int a,int b,int c,int d){
+            System.out.println(a+b+c+d);
+        }
+    }
+    public class Testoverloading{
+        public static void main(String args[]){
+            overloading s=new overloading();
+            s.add(1,2);
+            s.add(4,5,6);
+            s.add(1,3,4,9);
+        }
+    }
