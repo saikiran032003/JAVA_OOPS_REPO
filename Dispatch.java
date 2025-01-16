537157dd36fb3dd1779ae274f6d7d1bb053e2f25
saikiran032003 <165379895+saikiran032003@users.noreply.github.com>
2025-01-16 17:05:42 +0530

    Add files via upload

---
+++ Dispatch.java
@@ -0,0 +1,42 @@
+/*class A{
+    void callme()
+    {
+        System.out.println("hanuman");
+    }
+}
+class B extends A{
+    void callme(){
+        System.out.println("pavanasutanama anjaniputra");
+    }
+}
+class C extends B{
+    void callme(){
+        System.out.println("panchamuka hanuman");
+    }
+}
+class Dispatch{
+    public static void main(String args[]){
+        A a=new A();
+        B b=new B();
+        C c=new C();
+        A r;
+        r=a;
+        r.callme();
+        r=b;
+        r.callme();
+        r=c;
+        r.callme();
+    }
+}*/
+class A{
+        int x=10;
+}
+class B extends A{
+        int x=1099;
+}
+class Dispatch{
+    public static void main(String args[]){
+    A a=new B();
+    System.out.println(a.x);
+}
+}
