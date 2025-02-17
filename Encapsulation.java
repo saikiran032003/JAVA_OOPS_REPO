edec76e1d9d28d80cd461410db8e48b83f49756d
saikiran032003 <165379895+saikiran032003@users.noreply.github.com>
2025-02-17 20:30:28 +0530

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,18 +1,59 @@
- class Encapsulation {
-    int roll,marks;
-    String v;
-    Student(){
-        this.roll=1;
-        this.v="sai";
-        this.marks=10;
+class Encapsulation {
+    private int roll, marks;  // Private variables (data hiding)
+    private String v;
+
+    // Constructor
+    public Encapsulation() {
+        this.roll = 1;
+        this.v = "sai";
+        this.marks = 10;
     }
-    void greeting(){
-        System.out.println("my name is" +this.v);
+
+    // Getter methods (to access private variables)
+    public int getRoll() {
+        return roll;
     }
-    public static void main(String[] args){
-       Student obj=new Student();
-        System.out.println(obj.roll);
-        System.out.println(obj.v);
-        System.out.println(obj.marks);
+
+    public String getName() {
+        return v;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    // Setter methods (to modify private variables)
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setName(String v) {
+        this.v = v;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public void greeting() {
+        System.out.println("My name is " + this.v);
+    }
+
+    public static void main(String[] args) {
+        Encapsulation obj = new Encapsulation();
+
+        // Accessing data using getter methods (not directly)
+        System.out.println(obj.getRoll());
+        System.out.println(obj.getName());
+        System.out.println(obj.getMarks());
+
+        // Modifying data using setter methods
+        obj.setMarks(99);
+        System.out.println("Updated Marks: " + obj.getMarks());
     }
 }
+/*output
+1
+sai
+10
+Updated Marks: 99*/
